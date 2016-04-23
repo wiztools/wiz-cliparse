093321c685839f7bc3ab7b06ecda52f3f9cd57b6
Subhash Chandran <subwiz@gmail.com>
2016-04-23 19:51:17 +0530

    development.

--- test/test_parse.js
+++ test/test_parse.js
@@ -2,13 +2,17 @@ var expect = require('chai').expect;
 var program = require('../lib/program');
 
 describe("test parse", function(){
-  var args = ['-a', '--back', 'cmd', '-trace', 'arg1', 'arg2'];
+  var args = ['-a', '--back', 'cmd', '--trace', 'arg1', 'arg2'];
   it('checks parse', function(done){
-    var prg = new program.Program();
+    var prg = new program.Program('mycmd', 'description of mycmd.');
+    prg.addOpt('a', null, 'all option.');
+    prg.addOpt('b', 'back', 'back description.');
+    var cmd = prg.addCmd('cmd', 'cmd description.');
+    cmd.addOpt('t', 'trace', 'trace description.');
     var res = prg.parse(args);
     expect('cmd').to.equals(res.cmd);
-    expect(['-a', '--back']).to.deep.equals(res.gopts);
-    expect(['-trace']).to.deep.equals(res.opts);
+    expect(new Set(['a', 'b', 'back'])).to.deep.equals(res.gopts);
+    expect(new Set(['t', 'trace'])).to.deep.equals(res.opts);
     expect(['arg1', 'arg2']).to.deep.equals(res.args);
     done();
   });
