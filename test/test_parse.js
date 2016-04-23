53d640ce84f0b26b3f833b1e3b3a743800f45586
Subhash Chandran <subwiz@gmail.com>
2016-04-23 20:11:03 +0530

    simplification, and move to a more object based approach.

--- test/test_parse.js
+++ test/test_parse.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
-var program = require('../lib/program');
+var Program = require('../lib/program');
 
 describe("test parse", function(){
   var args = ['-a', '--back', 'cmd', '--trace', 'arg1', 'arg2'];
   it('checks parse', function(done){
-    var prg = new program.Program('mycmd', 'description of mycmd.');
+    var prg = new Program('mycmd', 'description of mycmd.');
     prg.addOpt('a', null, 'all option.');
     prg.addOpt('b', 'back', 'back description.');
     var cmd = prg.addCmd('cmd', 'cmd description.');
