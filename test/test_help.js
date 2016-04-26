56cccbe79dae2a37b488cdb7c3ac7076b324c7cc
Subhash Chandran <subwiz@gmail.com>
2016-04-26 17:44:07 +0530

    more code.

--- test/test_help.js
+++ test/test_help.js
@@ -14,9 +14,9 @@ describe('visual check of help', function(){
   var prg = new Program('mycmd', 'usage of mycmd.');
   prg.addOpt('a', null, 'all option.');
   prg.addOpt('b', 'back', 'back description.');
-  prg.addOpt('h', null, 'help option.');
   var cmd = prg.addCmd('cmd', 'cmd description.');
   cmd.addOpt('t', 'trace', 'trace description.');
+  prg.addHelp();
 
   var res = prg.parseSync(['-h']);
   prg.help(res, console.log);
@@ -25,12 +25,11 @@ describe('visual check of help', function(){
 
 describe('visual check of help with command', function(){
   var prg = new Program('mycmd', '[global-options] [command] [command-options]');
-  prg.addOpt('h', null, 'help option.');
-  prg.addHelpCmd();
   var cmd = prg.addCmd('dummy', 'dummy cmd desc.');
   cmd.addOpt('a', 'all', 'description of all option.');
+  prg.addHelp();
 
   var res = prg.parseSync(['help', 'dummy']);
-  prg.help(res, console.log);
+  prg.help(res);
   drawLine();
 });
